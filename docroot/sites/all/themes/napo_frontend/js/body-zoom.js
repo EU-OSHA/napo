683d099ba4ba300393a4caf93ceca0654c93822f
Claudia Ifrim <ifrim.claudia@gmail.com>
2015-04-02 16:11:06 +0300

    add css and javascript for set font size option

---
+++ docroot/sites/all/themes/napo_frontend/js/body-zoom.js
@@ -0,0 +1,9 @@
+function zoomSmall() {
+    jQuery("body").addClass("bodysmall");
+    jQuery("body").removeClass("bodybig");
+}
+
+function zoomBig() {
+    jQuery("body").addClass("bodybig");
+    jQuery("body").removeClass("bodysmall");
+}
