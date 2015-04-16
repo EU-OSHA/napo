87605ccd9d8409cd2153b498cc289b849dab20e4
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-04-16 12:27:07 +0300

    Content cart: added cart view and remove link field for views

--- docroot/sites/all/modules/napo/content_cart/js/content_cart.js
+++ docroot/sites/all/modules/napo/content_cart/js/content_cart.js
@@ -1,7 +1,7 @@
 (function($){
-
     Drupal.behaviors.content_cart_check_submit = {
         attach: function (context, settings) {
+            // Bind event for views checkbox - refresh the cart block.
             $('input.content_cart_checkbox', context).once('content_cart_check_submit', function () {
                 var id = $(this).val();
                 if (typeof id != 'undefined') {
@@ -10,6 +10,18 @@
                     ajax_settings.event = 'click';
                     var base = 'input.content_cart_checkbox';
                     Drupal.ajax['content_cart_submit'] = new Drupal.ajax(base, this, ajax_settings);
+                }
+            });
+            // Bind event for remove link. Refresh cart block and cart view.
+            $('.content_cart_remove', context).once('content_cart_remove_submit', function () {
+                var id = $(this).data('value');
+                var view = $(this).data('view');
+                if (typeof id != 'undefined' && typeof view != 'undefined') {
+                    var ajax_settings = {};
+                    ajax_settings.url = '/content_cart/ajax/submit/' + id + '/remove/' + view;
+                    ajax_settings.event = 'click';
+                    var base = '.content_cart_remove';
+                    Drupal.ajax['content_cart_remove'] = new Drupal.ajax(base, this, ajax_settings);
                 }
             });
         }
