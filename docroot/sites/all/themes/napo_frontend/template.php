e80e13c242f96201ff4e6bb605b2fe6848fc58b3
Radu Ciofringeanu <radu.sc@gmail.com>
2015-04-27 16:06:24 +0300

    NAPO-110 Back button

--- docroot/sites/all/themes/napo_frontend/template.php
+++ docroot/sites/all/themes/napo_frontend/template.php
@@ -1,3 +1,14 @@
 <?php
 
 require_once 'theme/menu/menu.inc';
+
+/**
+ * Implements hook_preprocess_node().
+ */
+function napo_frontend_preprocess_node(&$vars) {
+  $node = $vars['node'];
+
+  if($node->type == 'napo_video') {
+    $vars['back_button'] = l(t('Back to Napo\'s Films'), '/napos-films/films');
+  }
+}
