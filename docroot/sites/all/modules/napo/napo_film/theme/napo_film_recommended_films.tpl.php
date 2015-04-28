076645e9f4e422c6a01ff49a1b8492b01684305e
Radu Ciofringeanu <radu.sc@gmail.com>
2015-04-28 16:47:12 +0300

    NAPO-115-related-films Recommended 3 films

---
+++ docroot/sites/all/modules/napo/napo_film/theme/napo_film_recommended_films.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+// render recommended films from preprocess_node
+if (!empty($vars['recommended_films_nodes'])) { ?>
+  <div id="recommended_films">
+    <div class="recommended_films_head"><span><?php print t('Other films recommended for you');?><span></div>
+  </div>
+  <?php
+    foreach ($vars['recommended_films_nodes'] as $recommended) {
+      print render($recommended);
+    }
+}
