79bbcacbd4d7f72e219036b986b8524fa447dc23
Radu Ciofringeanu <radu.sc@gmail.com>
2015-05-04 18:05:15 +0300

    NAPO-150 Build top menu with related lessons

--- docroot/sites/all/modules/napo/napo_lesson/js/right_menu.js
+++ docroot/sites/all/modules/napo/napo_lesson/js/right_menu.js
@@ -34,4 +34,16 @@ jQuery('document').ready(function() {
             }
         }
     });
+
+    // Affix class to right nav menu
+    jQuery('#top_menu_lessons').affix({
+        offset: {
+            top: function () {
+                return (this.top = jQuery('#top_menu_lessons').parent().offset().top - 30);
+            },
+            bottom: function () {
+                return (this.bottom = jQuery('.footer').outerHeight(true) + 17);
+            }
+        }
+    });
 });
