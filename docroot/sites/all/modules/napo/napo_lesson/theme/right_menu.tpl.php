5ada9aed9e1725d9e9f8fef2741196f7c72d3534
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-07-14 17:37:13 +0300

    Fixed another translation of Lessons in left menu

--- docroot/sites/all/modules/napo/napo_lesson/theme/right_menu.tpl.php
+++ docroot/sites/all/modules/napo/napo_lesson/theme/right_menu.tpl.php
@@ -1,5 +1,5 @@
 <div class="scrollspy">
   <ul id="menu-about-page-menu" class="nav nav-right-menu nav-stacked">
-    <li class="headings">Lessons</li>
+    <li class="headings"><?php print t('Lessons')?></li>
   </ul>
 </div>
