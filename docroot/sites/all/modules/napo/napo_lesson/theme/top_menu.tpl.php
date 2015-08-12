594d0c49724f5027d9c6f58022c08680a12db2d5
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-08-12 16:00:11 +0300

    NAPO-491 tab label is Lesson

--- docroot/sites/all/modules/napo/napo_lesson/theme/top_menu.tpl.php
+++ docroot/sites/all/modules/napo/napo_lesson/theme/top_menu.tpl.php
@@ -2,7 +2,7 @@
   <ul id="top_menu_lessons" class="nav nav-right-menu nav-stacked">
     <?php foreach($lessons as $key => $lesson): ?>
       <li class="<?php echo $node->nid == $lesson->nid? 'lesson active':'lesson'; ?>">
-        <?php echo l(t('Part @no', array('@no' => ($key + 1))), 'node/'.$lesson->nid); ?>
+        <?php echo l(t('Lesson @no', array('@no' => ($key + 1))), 'node/'.$lesson->nid); ?>
       </li>
     <?php endforeach; ?>
   </ul>
