d4adbdf420137df28e3364edf50cea7f45851d4f
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-09-18 17:30:42 +0300

    NAPO-677 - donwload start auto

--- docroot/sites/all/modules/napo/napo_film/theme/napo_film_download_cart_complete.tpl.php
+++ docroot/sites/all/modules/napo/napo_film/theme/napo_film_download_cart_complete.tpl.php
@@ -1,7 +1,8 @@
 <?php ?>
-<div class="alert alert-success alert-dismissible" role="alert">
+<div class="hidden alert alert-success alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
   <p>
-  <a href="<?php print $file_path; ?>"><?php print $message; ?></a>
+  <?php /* <a href="<?php print $file_path; ?>"><?php print $message; ?></a> */ ?>
+    <script>location.replace('<?php print $file_path;?>');</script>
   </p>
 </div>
