84aac20d6c267f29154d7ad29424c990a4cce983
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-10-14 13:18:08 +0300

    Fix html error syntax in recomm films

--- docroot/sites/all/modules/napo/napo_film/theme/napo_film_recommended_films.tpl.php
+++ docroot/sites/all/modules/napo/napo_film/theme/napo_film_recommended_films.tpl.php
@@ -3,7 +3,7 @@
 // render recommended films from preprocess_node
 if (!empty($vars['recommended_films_nodes'])) { ?>
   <div id="recommended_films">
-    <div class="recommended_films_head"><span><?php print t('Other films recommended for you');?><span></div>
+    <div class="recommended_films_head"><?php print t('Other films recommended for you');?></div>
   </div>
   <?php
     foreach ($vars['recommended_films_nodes'] as $recommended) {
