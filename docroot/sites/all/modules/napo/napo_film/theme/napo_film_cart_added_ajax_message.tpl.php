93678a034c28d853828e1153cea8dad2f2683dd0
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-12-15 19:50:30 +0200

    Added notifications when add/remove from cart with video preview

---
+++ docroot/sites/all/modules/napo/napo_film/theme/napo_film_cart_added_ajax_message.tpl.php
@@ -0,0 +1,11 @@
+<div class="napo-film-added-to-cart-message">
+  <div class="napo-film-image">
+    <?php print $image; ?>
+  </div>
+  <div class="napo-film-title-field">
+    <?php print $title; ?>
+  </div>
+  <div class="napo-film-added-label">
+    <?php print $label; ?>
+  </div>
+</div>
