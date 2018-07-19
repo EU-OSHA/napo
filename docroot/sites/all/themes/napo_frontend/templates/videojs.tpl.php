d941739ec5e0f8d125ed3e9761367bfedd858d7d
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-07-19 18:35:13 +0300

    MDR-1977 add titles for the videos embeded internally

--- docroot/sites/all/themes/napo_frontend/templates/videojs.tpl.php
+++ docroot/sites/all/themes/napo_frontend/templates/videojs.tpl.php
@@ -3,7 +3,6 @@
  * @file
  * Provide the HTML output of the Video.js video player.
  */
-/**include("/home/sorin/napo/docroot/sites/all/libraries/video-js/video-js.css");*/
 $attrs = '';
 if (!empty($autoplay)) {
   $attrs .= ' autoplay="autoplay"';
@@ -11,18 +10,27 @@ if (!empty($autoplay)) {
 if (!empty($poster)) {
   $attrs .= ' poster="'. check_plain($poster) .'"';
 }
-
+if ($entity) {
+  $wrapper = entity_metadata_wrapper('node', $entity);
+  $title = $wrapper->title->value();
+  if ($title) {
+    $attrs .= ' data-matomo-title="'. check_plain($title) .'"';
+  }
+}
 if (!empty($items)): ?>
 <video id="<?php print $player_id; ?>-video" data-setup="{}" class="video-js vjs-default-skin"
        width="auto" height="<?php print($height) ?>"
        controls="controls" preload="metadata"
        <?php echo $attrs; ?>
        >
-<?php $ok=false; 
+<?php
+$ok=false;
 foreach ($items as $item): 
 	if (strcmp ( $item['videotype'] ,"video/mp4") == 0):?>
 		<source src="<?php print check_plain(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
-		<?php $ok=true; break; 
+		<?php
+      $ok=true;
+      break;
 	endif;?>
 <?php endforeach; 
 if($ok == false):
