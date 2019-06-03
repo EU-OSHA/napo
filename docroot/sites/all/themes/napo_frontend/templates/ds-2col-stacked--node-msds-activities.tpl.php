b528e5593cbef45d346db536d576cfb4efa869dd
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-06-03 16:48:28 +0300

    NRV-19 Napo MSDs Activities

---
+++ docroot/sites/all/themes/napo_frontend/templates/ds-2col-stacked--node-msds-activities.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+
+?>
+<section class="page--banner--block">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+    <div class="<?php print $header_classes; ?>">
+      <?php print $header; ?>
+    </div>
+</section>
+
+<section class="detail--section--block">
+    <h2><?php print t('Lesson'); ?></h2>
+    <div class="detail--text--block">
+        <div class="<?php print $left_classes; ?>">
+          <?php print $left; ?>
+        </div>
+        <div class="<?php print $header_classes; ?>">
+          <?php print $right; ?>
+        </div>
+    </div>
+</section>
