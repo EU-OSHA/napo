b9e2d2e156de48ad7ed301c4da1226a088c3cb18
ilorenzo10 <jilorenzo@bilbomatica.es>
2019-06-27 11:09:43 +0200

    Main menu open functionallity on mobile device

--- docroot/sites/all/themes/napo_frontend/js/frontpage-video.js
+++ docroot/sites/all/themes/napo_frontend/js/frontpage-video.js
@@ -206,15 +206,17 @@ jQuery(document).ready(function () {
             }
             
 
-            jQuery('header .menu li.dropdown > span').click(function () {
+            jQuery('header .menu li.dropdown > span').once().click(function () {
                 element = jQuery(this).parent();
                 jQuery('header .menu li.dropdown').removeClass('open');
 
                 if( element.hasClass('in') ){
                     jQuery('header .menu li.dropdown').removeClass('in');
+                    jQuery('.caret', this).removeClass('caret-up');
                 } else {
                     //jQuery('header .menu li.dropdown').removeClass('in');
                     jQuery( element ).addClass('in');
+                    jQuery('.caret', this).addClass('caret-up');
                 }            
             });
         
